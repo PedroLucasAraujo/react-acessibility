1021066fcbc3d52f8e109283988b66678c938f1f
Pedro Lucas Pinheiro Araujo <pedrolucas.araujo@agenciam3.com>
2024-01-15 22:27:40 -0300

    feat: testing the application and adding axe core to the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import "@/src/styles/globals.css";
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
+import { axeAccessibilityReporter } from "../utils/axeAccessibilityReporter";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    axeAccessibilityReporter();
+  }, []);
+
   return <Component {...pageProps} />;
 }
